c1012a539037922a73d1ce0f19e0a5962e7bb5e3
walerij <becwal@yandex.ru>
2017-11-28 16:11:39 +0300

    добавление usersuccess для корректного сохранения пользователя

---
+++ views/site/user/usersuccess.php
@@ -0,0 +1,16 @@
+<div class="jumbotron">
+    <h2>Спасибо!</h2>
+    <h3>Новый пользователь зарегистрирован в системе под именем nickname и под логином login</h3>
+    <h3>Вы можете открыть ему новый счет для hash-ей</h3>
+    <h3><a class="btn btn-success" href="#">Открыть счет</a></h3>
+</div>
+
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
